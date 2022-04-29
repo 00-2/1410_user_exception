d5adca7471e5dd1971ce3c3b9272651951f16e40
00-2 <9x02@mail.ru>
2022-04-29 19:08:10 +0300

    fix, shouldn't inheritance

--- InvalidPoint.h
+++ InvalidPoint.h
@@ -10,10 +10,10 @@
 #include "Point_In_Cube.h"
 
 class InvalidPoint
-        : std::out_of_range, Point_In_Cube {
+        : std::out_of_range {
     public:
     InvalidPoint(float x, float y, float z, const char *what)
-        : Point_In_Cube(0, 0, 0), std::out_of_range(what)
+        :  std::out_of_range(what)
         {
             std::cout << "Точка(x=" << x << ", y=" << y << ", z=" << z << ")не лежит внутри единичного куба" << std::endl
                 << "WHAT:" << what << std::endl;
